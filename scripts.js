f0379c1768be4bc2819bf727edb7b0ff6b3a207a
Just-Gio <JustG.io@outlook.it>
2022-12-22 11:38:42 +0100

    added beer image and fixed panel

--- scripts.js
+++ scripts.js
@@ -35,3 +35,9 @@ cocktails.forEach((cocktail) => {
     });
     }
 )
+
+//Icon hamburger
+let item = document.querySelector('.icon-hamburger');
+item.addEventListener("click", function(){
+    document.body.classList.toggle('menu-open');
+})
